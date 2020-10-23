3a1aadd4931fdc377f4618907cce8e60dd12c92d
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-23 20:00:46 +0700

    Add Update Functionality

--- routes/stories.js
+++ routes/stories.js
@@ -39,4 +39,45 @@ router.get('/', ensureAuth, async (req, res)=>{
   }
 });
 
+// @desc      Show edit page
+// @routes    GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res)=>{
+  const story = await Story.findOne({
+    _id: req.params.id
+  }).lean();
+
+  if (!story){
+    return res.render('error/404');
+  }
+
+  if (story.user != req.user.id){
+    res.redirect('/stories');
+  } else{
+    res.render('stories/edit', {
+      story,
+    });
+  }
+});
+
+// @desc      Update story
+// @routes    PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res)=>{
+  let story = await Story.findById(req.params.id).lean();
+
+  if (!story){
+    return res.render('error/404');
+  }
+
+  if (story.user != req.user.id){
+    res.redirect('/stories');
+  } else{
+    story = await Story.findOneAndUpdate( { _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.redirect('/dashboard');
+  }
+});
+
 module.exports = router;
