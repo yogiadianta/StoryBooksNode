84bc87b5e0eb33922f7c9cf7976e3cf10838b568
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-23 17:42:29 +0700

    Show all public stories in stories index

--- app.js
+++ app.js
@@ -28,8 +28,20 @@ if (process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
 
+// Handebars helpers
+const { formatDate } = require('./helpers/hbs');
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+  '.hbs', 
+  exphbs({ 
+    helpers: {
+      formatDate,
+    }, 
+    defaultLayout: 'main', 
+    extname: '.hbs',
+  })
+);
 app.set('view engine', '.hbs');
 
 // Session
