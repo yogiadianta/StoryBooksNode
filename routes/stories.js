d19d64ccf70d79f010ad9157a49c2eb3f3ce3c28
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-23 20:36:05 +0700

    Add Read More to show specific Stories

--- routes/stories.js
+++ routes/stories.js
@@ -39,6 +39,27 @@ router.get('/', ensureAuth, async (req, res)=>{
   }
 });
 
+// @desc      Show single story
+// @routes    GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res)=>{
+  try {
+    let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean();
+
+    if (!story){
+      return res.render('error/404');
+    }
+    
+    res.render('stories/show', {
+      story
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('error/404');
+  }
+});
+
 // @desc      Show edit page
 // @routes    GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res)=>{
